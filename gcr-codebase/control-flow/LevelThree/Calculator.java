8c82ae0e7a4694bfcd3642a8ff543026f5c9c34f
Adarsh Mishra <adarshengineer145@gmail.com>
2025-12-19 14:09:50 +0530

    [Adarsh]:Added a Calculator folder

---
+++ gcr-codebase/control-flow/LevelThree/Calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+
+   // Creating scanner Object  
+        Scanner sc = new Scanner(System.in);
+
+        // Taking inputs
+        System.out.print("Enter first number: ");
+        double first = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double second = sc.nextDouble();
+        
+        System.out.print("Enter operator (+, -, *, /): ");
+        String op = sc.next();
+
+        // Switch case to perform operation
+        switch (op) {
+
+            case "+":
+                System.out.println("Result = " + (first + second));
+                break;
+
+            case "-":
+                System.out.println("Result = " + (first - second));
+                break;
+
+            case "*":
+                System.out.println("Result = " + (first * second));
+                break;
+
+            case "/":
+                if (second != 0) {
+                    System.out.println("Result = " + (first / second));
+                } else {
+                    System.out.println("Division by zero is not allowed");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+        }
+
+        sc.close();
+    }
+}
