c58900adcd08f91c13513db0b632e10010361b8e
Adarsh Mishra <adarshengineer145@gmail.com>
2025-12-19 12:18:31 +0530

    [Adarsh]: Added a Greatest Factor folder

---
+++ gcr-codebase/control-flow/LevelTwo/GreatestFactor.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class GreatestFactor {
+    public static void main(String[] args) {
+      // CVreating Scanner Object
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+        int greatestFactor = 1;
+
+        // Check for positive integer
+        if (number <= 0) {
+            System.out.println("Please enter a positive integer.");
+        } else {
+            // Loop from number-1 down to 1
+            for (int i = number - 1; i >= 1; i--) {
+
+                // Check divisibility
+                if (number % i == 0) {
+                    greatestFactor = i;
+                    break;   // exit loop after finding greatest factor
+                }
+            }
+
+            // Display result
+            System.out.println("Greatest factor of " + number + " (excluding itself) is: " + greatestFactor);
+        }
+
+        sc.close();
+    }
+}
