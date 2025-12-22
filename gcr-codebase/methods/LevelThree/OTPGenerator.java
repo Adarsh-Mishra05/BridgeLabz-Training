82894e767048bab9ed2258fb7b90b88a397f3170
Adarsh Mishra <adarshengineer145@gmail.com>
2025-12-22 15:31:47 +0530

    [Adarsh]: Added OTP Genrator folder

---
+++ gcr-codebase/methods/LevelThree/OTPGenerator.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class OTPGenerator {
+   
+    // Generates numbers
+    public static int generateOTP() {
+        
+        return (int)(Math.random() * 900000) + 100000;
+    }
+    
+    // Method for check unique ids
+    public static boolean areOTPsUnique(int[] otps) {
+        for (int i = 0; i < otps.length; i++) {
+            for (int j = i + 1; j < otps.length; j++) {
+                if (otps[i] == otps[j]) {
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+   
+    public static void main(String[] args) {
+
+        int[] otps = new int[10];
+
+        for (int i = 0; i < otps.length; i++) {
+            otps[i] = generateOTP();
+        }
+       // Display of Results after checks
+        System.out.println("Generated OTPs:");
+        for (int otp : otps) {
+            System.out.println(otp);
+        }
+
+        if (areOTPsUnique(otps)) {
+            System.out.println("All OTPs are UNIQUE ");
+        } else {
+            System.out.println("Duplicate OTP found ");
+        }
+    }
+}
