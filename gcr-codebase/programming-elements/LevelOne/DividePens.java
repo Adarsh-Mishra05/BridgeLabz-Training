9bbde69fd19a46fecee332cbceb3b51d619610dd
Adarsh Mishra <149897192+Adarsh-Mishra05@users.noreply.github.com>
2025-12-26 17:30:16 +0530

    [Adarsh] : deleted unused files

--- gcr-codebase/programming-elements/LevelOne/DividePens.java
+++
@@ -1,8 +0,0 @@
-public class DividePens{
-	 public static void main(String[] args){
-	      int per_person=14/3;
-	      int rem =14%3;
-
-	      System.out.println("The Pen Per Student is "+ per_person + " and the remaining pen not distributed is "+ rem);
-	 }
-}
