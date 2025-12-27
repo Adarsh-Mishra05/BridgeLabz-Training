e3cce9346d4766cd4e118a8d731a5f4cacdf9f10
Adarsh Mishra <adarshengineer145@gmail.com>
2025-12-27 12:46:03 +0530

    [Adarsh]: Added Area of Circle Program

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobjects/level1/circle.java
@@ -0,0 +1,40 @@
+package com.classandobjects.level1;
+
+public class Circle {
+	 
+	 // Attribute
+    double radius;
+
+    // Method to calculate area
+    void calculateArea() {
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of the Circle = " + area);
+    }
+
+    // Method to calculate circumference
+    void calculateCircumference() {
+        double circumference = 2 * Math.PI * radius;
+        System.out.println("Circumference of the Circle = " + circumference);
+    }
+}
+
+public class CircleDemo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Create object of Circle
+        Circle c = new Circle();
+
+        // Take radius input
+        System.out.print("Enter radius of the circle: ");
+        c.radius = sc.nextDouble();
+
+        // Call methods
+        c.calculateArea();
+        c.calculateCircumference();
+
+        sc.close();
+    }
+
+}
