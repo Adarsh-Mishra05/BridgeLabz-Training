3abb8841815f16deb8e6e707bbd442d89b3b8013
Adarsh Mishra <adarshengineer145@gmail.com>
2025-12-28 12:49:27 +0530

    [Adarsh]: Added Book Folder

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Book.java
@@ -0,0 +1,16 @@
+package com.constructor.level1;
+
+public class Book {
+	String title, author;
+	double price;
+	
+	Book(String title, String author, double price){
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+	void DisplayBook() {
+		System.out.println("Book Title: "+ title +"\nAuthor Name: "+ author +"\nBook Price: "+price);
+	}
+
+}
