5f96fc0c10497d9ffc3e86b1e6a8a588fce9dd18
Adarsh Mishra <adarshengineer145@gmail.com>
2025-12-28 13:00:10 +0530

    [Adarsh]: Added Car Rental Folder

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/CarRental.java
@@ -0,0 +1,41 @@
+package com.constructor.level1;
+
+public class CarRental {
+	
+	    String customerName;
+	    String carModel;
+	    int rentalDays;
+	    double costPerDay = 1500;
+
+	    // Default Constructor
+	    CarRental() {
+	        customerName = "Customer";
+	        carModel = "Basic";
+	        rentalDays = 1;
+	    }
+
+	    // Parameterized Constructor
+	    CarRental(String customerName, String carModel, int rentalDays) {
+	        this.customerName = customerName;
+	        this.carModel = carModel;
+	        this.rentalDays = rentalDays;
+	    }
+
+	    double calculateTotalCost() {
+	        return rentalDays * costPerDay;
+	    }
+
+	    void display() {
+	        System.out.println(customerName + " | " + carModel + 
+	        " | Total Cost: ₹" + calculateTotalCost());
+	    }
+
+	    public static void main(String[] args) {
+	        CarRental c1 = new CarRental();
+	        CarRental c2 = new CarRental("Priyanshu", "SUV", 4);
+
+	        c1.display();
+	        c2.display();
+
+}
+}
