498e9d746b2875770b7951ec06a2273b6f29a8d0
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-01 11:54:26 +0530

    [Adarsh]:Added Animal Hierarchy Folder

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/AnimalHierarchy/Animal.java
@@ -0,0 +1,54 @@
+package com.inheritance.AnimalHierarchy;
+
+import com.inheritance.Animal;
+import com.inheritance.Bird;
+import com.inheritance.Cat;
+import com.inheritance.Dog;
+
+
+	class Animal{
+		String name;
+		int age;
+		
+		void makeSound() {
+			System.out.println("Animals make sound");
+		}
+	}
+
+	class Dog extends Animal{
+		void makeSound() {
+			System.out.println("Dog make sound");
+		}
+	}
+
+	class Cat extends Animal{
+		void makeSound() {
+			System.out.println("Cat make sound");
+		}
+	}
+
+	class Bird extends Animal{
+		void makeSound() {
+			System.out.println("Bird make sound");
+		}
+	}
+
+	public class Animal{
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Bird bird = new Bird();
+			Cat cat = new Cat();
+			Dog dog = new Dog();
+			Animal animal = new Animal();
+			
+			bird.makeSound();
+			cat.makeSound();
+			dog.makeSound();
+			animal.makeSound();
+
+		}
+	
+
+}
