53673939614827aea522d4f3c7d39e92af74e6a7
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-05 14:43:42 +0530

    [Adarsh]:Added Check for Pair with Sum Array Folder

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/hashmapandhashfunctions/CheckforaPairwithGivenSumArray/PairWithGivenSumMain.java
@@ -0,0 +1,11 @@
+package hashmapandhashfunctions.CheckforaPairwithGivenSumArray;
+
+public class PairWithGivenSumMain {
+    public static void main(String[] args) {
+
+        int[] arr = {8, 7, 2, 5, 3, 1};
+        int target = 10;
+
+        PairWithGivenSum.hasPair(arr, target);
+    }
+}
