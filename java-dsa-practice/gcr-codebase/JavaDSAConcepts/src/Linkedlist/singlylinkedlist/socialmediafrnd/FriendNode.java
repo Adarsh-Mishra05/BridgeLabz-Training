0edfba2edf5306e15c341e5fde07693aab4a3fdd
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-05 12:59:48 +0530

    [Adarsh]:Added Social Media Friend Folder

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/Linkedlist/singlylinkedlist/socialmediafrnd/FriendNode.java
@@ -0,0 +1,11 @@
+package Linkedlist.singlylinkedlist.socialmediafrnd;
+
+public class FriendNode {
+    int friendId;
+    FriendNode next;
+
+    FriendNode(int friendId) {
+        this.friendId = friendId;
+        this.next = null;
+    }
+}
