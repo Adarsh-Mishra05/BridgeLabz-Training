bc1ebd7131916ce21b7f6fd6a7e63ece69db96da
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-08 12:00:07 +0530

    [Adarsh]:Added Concatenate folder

---
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/stringbuffer/Concatenate.java
@@ -0,0 +1,16 @@
+package stringbuffer;
+
+public class Concatenate {
+public static void main(String args[]) {
+		
+		String[] arr = {"Harsh " + "is " + "a " + "Thinker"};
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(String s : arr) {
+			sb.append(s);
+		}
+		
+		System.out.println(sb.toString());
+}
+}
