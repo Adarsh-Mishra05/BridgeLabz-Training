ee3c4ebbc7811363f06d6e36caca39befa4512ee
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-12 15:16:30 +0530

    [Adarsh]:Added BookShelf Folder

---
+++ java-dsa-practice/scenerio-based-codebase/DSAScenerioBased/src/Week1/BookShelf/Book.java
@@ -0,0 +1,13 @@
+package Week1.BookShelf;
+
+public class Book {
+	String title;
+	String author;
+	public Book(String title, String author) {
+		//super();
+		this.title = title;
+		this.author = author;
+	}
+	
+	
+}
