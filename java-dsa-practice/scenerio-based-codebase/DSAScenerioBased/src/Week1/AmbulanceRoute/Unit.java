d955de1eb9cc82c59828d8667832ba9a30139a54
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-12 13:08:17 +0530

    [Adarsh]:Added Ambulance Route Folder

---
+++ java-dsa-practice/scenerio-based-codebase/DSAScenerioBased/src/Week1/AmbulanceRoute/Unit.java
@@ -0,0 +1,12 @@
+package Week1.AmbulanceRoute;
+
+public class Unit {
+	 String name;
+	    boolean available = true;
+	    Unit next;
+
+	    Unit(String name) {
+	        this.name = name;
+	    }
+
+}
