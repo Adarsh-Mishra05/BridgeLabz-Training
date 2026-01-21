fe52b19844c7a6a04717f36103697a7af42c92c2
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-21 15:37:44 +0530

    Added Queue Interface Folder

---
+++ Java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/queueinterface/Hospitaltriage/Main.java
@@ -0,0 +1,16 @@
+package queueinterface.Hospitaltriage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HospitalTriage triage = new HospitalTriage();
+
+        triage.addPatient(new Patient("John", 3));
+        triage.addPatient(new Patient("Alice", 5));
+        triage.addPatient(new Patient("Bob", 2));
+
+        System.out.println("Treatment Order:");
+        triage.treatPatients();
+    }
+}
---
+++ Java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/queueinterface/Hospitaltriage/Patient.java
@@ -0,0 +1,11 @@
+package queueinterface.Hospitaltriage;
+
+public class Patient {
+    String name;
+    int severity;
+
+    public Patient(String name, int severity) {
+        this.name = name;
+        this.severity = severity;
+    }
+}
