88c763a8a9e99658a12cd59796164977c146e739
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-21 12:30:46 +0530

    [Adarsh]: Added Edu Results Folder

---
+++ java-dsa-practice/scenerio-based-codebase/DSAScenerioBased/src/Week2/EduResults/Student.java
@@ -0,0 +1,11 @@
+package Week2.EduResults;
+
+class Student {
+    int marks;
+    String name;
+
+    Student(int marks, String name) {
+        this.marks = marks;
+        this.name = name;
+    }
+}
