ce45d630bfad126d5b2fefa7b241931c25f435f8
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-24 15:15:26 +0530

    [Adarsh]:Added Read User Input Folder

---
+++ Java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/javastreams/ReadUserInput/Main.java
@@ -0,0 +1,10 @@
+package javastreams.ReadUserInput;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String fileName = "user_details.txt";
+        UserInputUtil.takeInputAndSave(fileName);
+    }
+}
