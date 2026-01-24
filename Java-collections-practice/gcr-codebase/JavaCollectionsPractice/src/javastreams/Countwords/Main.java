4bcbbe4677e5f1e3cb5e5aef225b8ca5d988fb3f
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-24 15:06:33 +0530

    [Adarsh]:Added Count Word Folder

---
+++ Java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/javastreams/Countwords/Main.java
@@ -0,0 +1,10 @@
+package javastreams.Countwords;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filePath = "input.txt";
+        WordCountUtil.countTopWords(filePath);
+    }
+}
