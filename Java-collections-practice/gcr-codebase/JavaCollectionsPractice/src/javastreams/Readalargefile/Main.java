60929edc6eebbf4e4c4480a10c8bed405bbf969f
Adarsh Mishra <adarshengineer145@gmail.com>
2026-01-24 15:15:55 +0530

    [Adarsh]:Added Read Large File Folder

---
+++ Java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/javastreams/Readalargefile/Main.java
@@ -0,0 +1,11 @@
+package javastreams.Readalargefile;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String largeFilePath = "large_log_file.txt"; // 500MB+
+
+        ErrorLogReader.readErrorLines(largeFilePath);
+    }
+}
