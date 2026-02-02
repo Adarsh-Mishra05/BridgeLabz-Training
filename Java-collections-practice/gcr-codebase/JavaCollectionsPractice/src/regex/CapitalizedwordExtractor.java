b03fcff2ee485958931c9bccbbd616bb580898d4
Adarsh Mishra <adarshengineer145@gmail.com>
2026-02-02 12:16:21 +0530

    [Adarsh]: Added Captalized Word Extractor folder

---
+++ Java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/regex/CapitalizedwordExtractor.java
@@ -0,0 +1,25 @@
+package regex;
+import java.util.regex.*;
+
+public class CapitalizedwordExtractor {
+
+    public static void main(String[] args) {
+
+        String text = "The Eiffel Tower is in Paris and the Statue of Liberty is in New York.";
+
+        String regex = "\\b[A-Z][a-z]*\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            String word = matcher.group();
+
+            // optional: skip "The"
+            if (!word.equals("The")) {
+                System.out.println(word);
+            }
+        }
+    }
+}
+
