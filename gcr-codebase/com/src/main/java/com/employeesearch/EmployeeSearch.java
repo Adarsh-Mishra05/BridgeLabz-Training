4edca3722a3c11ded57450dff213a02c81303161
Adarsh Mishra <adarshengineer145@gmail.com>
2026-02-03 16:46:38 +0530

    [Adarsh]: Added Employee Search Folder

---
+++ gcr-codebase/com/src/main/java/com/employeesearch/EmployeeSearch.java
@@ -0,0 +1,37 @@
+package com.employeesearch;
+
+import java.io.*;
+import java.util.Scanner;
+/*
+ * Intermediate Problems
+5️. Search for a Record in CSV
+Read an employees.csv file and search for an employee by name.
+Print their department and salary.
+
+ */
+public class EmployeeSearch {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Employee Name to search: ");
+		String searchName = sc.nextLine();
+
+		try (BufferedReader br = new BufferedReader(new FileReader("/Users/apple/Desktop/java programming workspace/gcr-codebase/com/src/main/resources/employees.csv"))) {
+			String line;
+			boolean found = false;
+			br.readLine(); // Skip header
+
+			while ((line = br.readLine()) != null) {
+				String[] data = line.split(",");
+				if (data[1].equalsIgnoreCase(searchName)) {
+					System.out.println(" !! Found: Dept: " + data[2] + ", Salary: Rs." + data[3]);
+					found = true;
+					break;
+				}
+			}
+			if (!found)
+				System.out.println(" Employee not found.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
