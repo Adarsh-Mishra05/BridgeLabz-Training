01501bc57af7a03beb885efff3dc8501e1bee983
Adarsh Mishra <adarshengineer145@gmail.com>
2026-02-09 15:30:33 +0530

    [Adarsh]:Added Employee Management Folder

---
+++ gcr-codebase/mysqlpractice/src/main/java/com/mysqlpractice/App.java
@@ -0,0 +1,13 @@
+package com.mysqlpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
